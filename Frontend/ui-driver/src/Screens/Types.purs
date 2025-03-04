deec865cbcba128fddd9946f598bced487d2df0b
Shree Harish S <harish.s@nammayatri.in>
2025-03-04 17:25:08 +0530

    frontend/feat : Made rideInitScreen and rideStartedScreen for MeterRideScreen

--- Frontend/ui-driver/src/Screens/Types.purs
+++ Frontend/ui-driver/src/Screens/Types.purs
@@ -3486,7 +3486,6 @@ type MeterRideScreenState = {
 
 type MeterRideScreenData = {
   distance :: Number,
-  timeMin :: Int,
   timeSec :: Int,
   destinationAddress :: String,
   destinationLat :: Number,
@@ -3502,6 +3501,6 @@ type MeterRideScreenProps = {
   sliderDefVal :: Int,
   incrementUnit :: Int,
   sliderVal :: Int,
-  ratePerKM :: Int,
+  ratePerKM :: Number,
   isMeterRideStarted :: Boolean
 }
